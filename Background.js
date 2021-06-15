583894cd15ac4ca3403132969db61e0fc9b2e0d8
Karen Méndez <kmendezgz@gmail.com>
2021-06-14 18:04:56 -0600

    clear background

--- Background.js
+++ Background.js
@@ -1,7 +1,7 @@
 export default class Background {
     color = '';
-    canvas = document.querySelector("#canvas");;
-    ctx = canvas.getContext("2d");;
+    canvas = '';
+    ctx = '';
     color = '';
     width = '';
     height = '';
